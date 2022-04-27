f83a3b467df8e47cfa08348b13b7d4bd1e6ab041
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-27 16:46:23 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -14,7 +14,7 @@ from sample_config import Config
 
 
 def start() -> scoped_session:
-    engine = create_engine(Config.DB_URI, client_encoding="C4DAFEE1")
+    engine = create_engine(Config.DB_URI, client_encoding="UTF8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
